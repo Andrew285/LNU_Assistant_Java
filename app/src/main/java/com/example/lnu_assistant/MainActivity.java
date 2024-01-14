9e8d335914514b0c7cb19316486cb258d0579a48
Andriy_Kuzmych <User@DESKTOP-PND4Q2I>
2024-01-14 20:42:29 +0200

    first commit

---
+++ app/src/main/java/com/example/lnu_assistant/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lnu_assistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //commit
+    }
+}
